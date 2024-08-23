ce8e36787de811f6f558cddd05d78ec50bf4383e
mehthishamkhan06 <mehthishamkhan06@gmail.com>
2024-08-23 23:39:22 +0400

    minitalk

--- client.c
+++ client.c
@@ -1,22 +1,67 @@
 #include <unistd.h>
 #include <stdio.h>
 #include	<ctype.h>
+#include <signal.h>
 
 
+//SIGUSER1: represents bit value 1
+//SIGUSER2: represents bit value 0
+int	is_digit_str(char *str)
+{
+	int	i;
+
+	i = 0;
+	while(str[i])
+	{
+		if (!ft_isdigit[i])
+			return (0);
+		i++;
+	}
+	return (1);
+}
+
+void    send_signals(int pid, char c)
+{
+	int a;
+	int b;
+	
+	a = 0;
+	b = 7;
+	while(b-- >= 0)
+	{
+		if ((c >> i && 1) == 0)
+		{
+			j = kill(pid, SIGUSR2);
+			if (j == -1)
+				return(ft_putendl_fd("ERROR", 2),exit(1),0);  
+		}
+		else
+		{
+			j = kill(pid, SIGUSR1);
+			if (j == -1)
+				return(ft_putendl_fd("ERROR", 2),exit(1),0);
+		}
+		usleep(150);
+	}
+}
 int main(int argc, char *argv[])
 {
-    int pid;
-    int i = 0;
+	int pid;
+	int i = 0;
 
-    if (argc == 3)
-    {
-        if (is_alpha(argv[1]) != 1)
-            //print error
-        // extract pid to readable using atoi
-        pid = ft_atoi(argv[1]);
-        //converrt the pid to signals so the the server can understand
-
-    }
-    else
-        //print error message
+	if (argc == 3)
+	{
+		if (ft_isdigit(argv[1]) != 1) //replace is digit with your own func as ft_isdigit doesnt check for complete string.
+			//print error
+			return(ft_putendl_fd("ERROR", 2), 0);// for safety..)
+		// extract pid to readable using atoi
+		pid = ft_atoi(argv[1]);
+		while(argv[2][i])
+			send_signals(pid, argv[2][i++]); //create a send signals functions
+		send_signals(pid, '\0');
+	}
+	else
+		//print error message
+		ft_putendl_fd("ERROR", 2);
+	return (0);
 }
--- server.c
+++ server.c
@@ -1,10 +1,25 @@
+#include <unistd.h>
+#include <ctype.h>
+#include <stdio.h>
+#include <sys/types.h>
+#include <signal.h>
 int main(int argc, char *argv[])
 {
-    int pid;
+	int pid;
 
-    (void)argv;
-    if (argc == 1)
-        //print the pid with newline and wait for the messages from client
-    else
-        //print error too few commands.
+	(void)argv;
+	if (argc == 1)
+	{
+		pid = getpid();
+		ft_putstr_fd("PID: ", 1); // print the pid with newline and wait for the messages from client
+		ft_putnbr_fd(pid, 1);
+		ft_putchar_fd('\n', 1);
+		signal(SIGUSR1, receiver);
+		signal(SIGUSR2, receiver);
+		while (1)
+			pause();
+	}
+	else
+		ft_putendl_fd("ERROR", 2); // print error too few commands.
+	return (0);
 }
